e7ef9300819555487b2010521f505bdd2ffa4d37
Makis Otman <makisotman@gmail.com>
2016-08-14 18:05:31 +0100

    Merge GitFileStats into FileStats
    It's one idea -> stats on a file.

--- test/file_stats_test.exs
+++ test/file_stats_test.exs
@@ -5,4 +5,12 @@ defmodule FileStatsTest do
   test "can provide the line count of a file" do
     assert FileStats.line_count("test/test_file.txt") == 4
   end
+
+  test "retrieves the commits for a given file" do
+    commits = FileStats.file_commits("test/test_file.txt")
+
+    assert 2 = length(commits)
+    assert "commit" <> _sha = List.first(commits)
+    assert "commit" <> _sha = List.last(commits)
+  end
 end
