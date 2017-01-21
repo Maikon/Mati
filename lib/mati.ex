ae6827b27017071938e73edd2dd0e715b487707e
Makis Otman <makisotman@gmail.com>
2017-01-21 13:13:49 +0000

    Print usage instructions when --help is provided

--- lib/mati.ex
+++ lib/mati.ex
@@ -2,30 +2,27 @@ defmodule Mati do
   alias Mati.Screen
 
   def main(args) do
-    {parsed_args, _remaining, _invalid} = OptionParser.parse(args)
-
-    case Keyword.fetch(parsed_args, :target) do
-      {:ok, _dir} -> print_table(parsed_args)
-      :error -> print_table()
-    end
+    OptionParser.parse(args) |> print_table
   end
 
-  defp print_table() do
+  defp print_table({[help: true], _remaining, _invalid}) do
     IO.puts """
-    Please provide a directory to analyse. Examples:
+    Usage:
 
     1. mati --target .
 
-    2. mati --target lib --ignore png,jpg,txt
+    2. mati --target . --ignore png,jpg,txt
 
-    3. mati --target . --files 20
+    3. mati --target lib --files 20
 
-    4. mati --target . --files all
+    4. mati --target lib --files all
+
+    5. mati --target . --ignore png,jpg --files all
     """
   end
 
-  defp print_table(args) do
-    Screen.display_table(args)
+  defp print_table({valid_args, _remaining, _invalid}) do
+    Screen.display_table(valid_args)
     |> IO.puts
   end
 end
