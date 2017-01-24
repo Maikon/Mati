052ff2495383aebf0992616c1d8ce8898d00771d
Makis Otman <makisotman@gmail.com>
2017-01-24 09:25:52 +0000

    Fix table flexibility and layout

--- lib/table.ex
+++ lib/table.ex
@@ -1,10 +1,10 @@
 defmodule Mati.Table do
   alias IO.ANSI
   require Integer
-  @default_max_width 22
+  @default_max_width 15
 
   def build_table(files) do
-    longest_filename = longest_filename_with_padding(files)
+    longest_filename = longest_filename(files)
     {header_size, header_with_separator} = build_header(longest_filename)
 
     Enum.reduce(files, [header_with_separator], fn(file, table) ->
@@ -14,7 +14,7 @@ defmodule Mati.Table do
 
   defp construct_line_from_file(file, longest_filename, header_size) do
     [
-      build_column(clean_filename(file.name), longest_filename)
+      build_file_column(clean_filename(file.name), longest_filename)
       <> "|"
       <> build_column(to_string(file.line_count))
       <> "|"
@@ -27,19 +27,29 @@ defmodule Mati.Table do
     ]
   end
 
-  defp longest_filename_with_padding([]), do: @default_max_width
-  defp longest_filename_with_padding(files) do
-    extra_padding = 5
-
+  defp longest_filename([]), do: @default_max_width
+  defp longest_filename(files) do
     Enum.max_by(files, fn(file) ->
       String.length(file.name)
     end).name
     |> String.length
-    |> Kernel.+(extra_padding)
   end
 
   defp build_header(longest_filename) do
-    header =
+    header = header(longest_filename)
+
+    header_size = String.length(header) - String.length(cyan("")) * 3 - 3
+    header_with_separator = yellow(replicate("=", header_size))
+    <> "\n"
+    <> header
+    <> "\n"
+    <> yellow(replicate("=", header_size))
+    <> "\n"
+
+    {header_size, header_with_separator}
+  end
+
+  defp header(longest_filename) do
     cyan(build_column("File", longest_filename))
     <> "|"
     <> cyan(build_column("Line Count"))
@@ -47,29 +57,36 @@ defmodule Mati.Table do
     <> cyan(build_column("Commits"))
     <> "|"
     <> cyan(build_column("Last Modified"))
-
-    header_size = String.length(header)
-    header_with_separator = yellow(replicate("=", header_size)) <> "\n" <>
-    header <> "\n" <> yellow(replicate("=", header_size)) <> "\n"
-
-    {header_size, header_with_separator}
   end
 
   defp clean_filename(filename) do
     String.split(filename, "./") |> to_string
   end
 
+  defp build_file_column(column_value, max_width) do
+    {padding, extra_padding} = padding(column_value, max_width)
+
+    column_value
+    <> replicate(" ", extra_padding + padding * 2)
+  end
+
   defp build_column(column_value, max_width \\ @default_max_width) do
-    remaining_space = max_width - String.length(column_value)
-    padding = div(remaining_space, 2)
-    extra_padding = rem(remaining_space, 2)
+    {padding, extra_padding} = padding(column_value, max_width)
 
     replicate(" ", padding)
     <> column_value
     <> replicate(" ", padding + extra_padding)
   end
 
-  defp replicate(symbol, count) when count < 0, do: String.duplicate(symbol, 5)
+  defp padding(column_value, max_width) do
+    remaining_space = max_width - String.length(column_value)
+    padding = div(remaining_space, 2)
+    extra_padding = rem(remaining_space, 2)
+
+    {padding, extra_padding}
+  end
+
+  defp replicate(symbol, count) when count < 0, do: String.duplicate(symbol, 1)
   defp replicate(symbol, count), do: String.duplicate(symbol, count)
 
   defp cyan(str), do: colourize(ANSI.cyan, str)
